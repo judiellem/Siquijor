fdc963b3321c52400f930812e05559a5639d478a
Joshua James Dela Cruz <judu.magdaleno.sjc@phinmaed.com>
2023-11-28 02:18:39 +0800

    test
    test

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,13 @@ import bgVideo from '../assets/beachVid.mp4';
 const Hero = () => {
   return (
     <header className='w-screen h-screen relative'>
-
+      <video
+        src={bgVideo}
+        className='w-full h-full object-cover'
+        autoPlay
+        loop
+        muted
+      />
       <div className='absolute top-0 left-0 w-full h-full bg-gray-900/30'></div>
       <div className='absolute top-0 left-0 w-full h-full flex flex-col justify-center text-center'>
         <h1 className='text-white mb-2'>Siquijor</h1>
