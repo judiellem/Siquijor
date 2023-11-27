f2e380ebb73fa8c6e9a8964a524979f25a4dc439
Joshua James Dela Cruz <judu.magdaleno.sjc@phinmaed.com>
2023-11-28 01:57:16 +0800

    test
    test

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Destinations from './components/Destinations';
 import Search from './components/Search';
-import Selections from './components/Selections';
 import Attractions from './components/Attractions';
 import Carousel from './components/Carousel';
 import Footer from './components/Footer';
