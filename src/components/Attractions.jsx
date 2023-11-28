325c80ee1aa88d3b9e412185e7aa2463770b3644
Joshua James Dela Cruz <judu.magdaleno.sjc@phinmaed.com>
2023-11-28 14:45:04 +0800

    test
    test

--- src/components/Attractions.jsx
+++ src/components/Attractions.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Foods from '../assets/Foods.jpg';
 import Celebrations from '../assets/Celebrations.jpg';
 import locations from '../assets/locations.jpg';
-import hotspots from '../assets/hotspots.jpg';
+import Hotspots from '../assets/Hotspots.jpg';
 
 const Attractions = () => {
   return (
